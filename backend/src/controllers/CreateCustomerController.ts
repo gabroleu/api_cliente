bdc11cf7783b0f4387f6a9c6e5969c3368a93f73
gabroleu <rgabrielcs@gmail.com>
2026-01-27 14:51:08 -0400

    atualização backed, prisma configurado e rotas

---
+++ backend/src/controllers/CreateCustomerController.ts
@@ -0,0 +1,20 @@
+import {FastifyRequest, FastifyReply} from 'fastify'
+
+import { CreateCustomerService } from '../services/CreateCustomerService'
+
+class CreateCustomerController{
+    async handle(request: FastifyRequest, reply:FastifyReply){
+        const { name, email } = request.body as { name: string, email: string};
+        
+
+        const customerService = new CreateCustomerService()
+
+        const customer = await customerService.execute();
+
+        reply.send(customer)
+
+    }
+}
+
+export { CreateCustomerController }
+
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,17 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify"
+import { CreateCustomerController } from '../src/controllers/CreateCustomerController'
 
-export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){
 
-    fastify.get("/teste", async(request: FastifyRequest, reply: FastifyReply) => {
-        return {ok: true}
+export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions){ //declarando uma função plugin do Fastify
 
+    fastify.get("/teste", async(request: FastifyRequest, reply: FastifyReply) => { //registrando uma rota GET no endpoint
+        return {ok: true} //retornando um objeto JSON como resposta HTTP
+
+
+    })
+
+    fastify.post("/customer", async ( request: FastifyRequest, reply:FastifyReply) => {
+        return new CreateCustomerController().handle(request, reply)
 
     })
 
