4d131bc4c936d8e8114a38ca666267d63a26e5f6
gabroleu <rgabrielcs@gmail.com>
2026-03-31 12:24:46 -0400

    errx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="w-full min-h-screen bg-gray-800 flex justify-center px-4">
       < main className="my-10 w-full">
-      <h1 className="text-4xl font-medium text-blue-500">Clientes</h1>
+      <h1 className="text-4xl font-medium text-blue-500">API Clientes</h1>
       </main>
   
     </div>
