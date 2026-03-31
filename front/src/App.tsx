fa21653b54c3f8ca98189c6614c1b598c773ffab
gabroleu <rgabrielcs@gmail.com>
2026-03-31 15:37:52 -0400

    front

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="w-full min-h-screen bg-gray-800 flex justify-center px-4">
+    <main className="my-10 w-full md:max-w-10xl">
+    <h1 className="text-4xl font-medium text-white">Clientes</h1>
+    </main>
+    </div>
+
+  )
+}
