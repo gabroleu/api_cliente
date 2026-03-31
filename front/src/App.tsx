1e294d294045851c55284288dd97aa954e87b242
gabroleu <rgabrielcs@gmail.com>
2026-03-31 17:21:05 -0400

    create input

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,31 @@
 export default function App() {
   return (
-    <div className="w-full min-h-screen bg-gray-800 flex justify-center px-4">
-    <main className="my-10 w-full md:max-w-10xl">
+    <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+    <main className="my-10 w-full md:max-w-2xl">
     <h1 className="text-4xl font-medium text-white">Clientes</h1>
+
+    <form className="flex flex-col my-6">
+        <label className="font-medium text-white">Nome:</label>
+        <input
+        type="text"
+        placeholder="Digite seu nome completo..."
+        className="border border-gray-400 w-full mb-5 p-2 rounded"
+        />
+
+        <label className="font-medium text-white">Email:</label>
+        <input
+        type="email"
+        placeholder="Digite seu email..."
+        className="border border-gray-400 w-full mb-5 p-2 rounded"
+        />
+
+        <input
+        type="submit"
+        value="Cadastrar"
+        className="cursor-pointer w-full p-2  bg-green-500 rounded font-medium"
+        />
+
+    </form>
     </main>
     </div>
 
