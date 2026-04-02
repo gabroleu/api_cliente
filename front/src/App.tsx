926f972359fab15ceae09ebb6a06759196726490
gabroleu <rgabrielcs@gmail.com>
2026-04-01 23:30:56 -0400

    searching for customers in the database

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,32 @@ import {FiTrash} from 'react-icons/fi'
 import { api} from './services/api'
 
 
+interface CustomerProps{
+  id: string;
+  name: string;
+  email: string;
+  status: boolean;
+  created_at: string;
+}
+
+
 export default function App() {
+
+    const [customers, setCustomers] = useState<CustomerProps[]>([])
+
+    useEffect(() => {
+      loadCustomers();
+    }, [])
+
+
+
+    async function loadCustomers(){
+      const response = await api.get("/customers")
+      setCustomers(response.data);
+    }
+
+
+
   return (
     <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
     <main className="my-10 w-full md:max-w-2xl">
@@ -34,23 +59,26 @@ export default function App() {
 
 
 
-    <section className="flex flex-col">
+    <section className="flex flex-col gap-4">
 
+     {customers.map( (customer) => (
       <article
-      className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200"
+      key={customer.id}
+    className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200"
+    >
+      <p><span className="font-medium">Nome:</span> {customer.name} </p>
+      <p><span className="font-medium">Email:</span> {customer.email}</p>
+      <p><span className="font-medium">Status:</span> {customer.status ? "ATIVO" : "INATIVO"}</p>
+
+
+      <button
+        className='bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2'
       >
-        <p><span className="font-medium">Nome:</span> Gabriel</p>
-        <p><span className="font-medium">Email:</span> rgabrielcs@gmail.com</p>
-        <p><span className="font-medium">Status:</span> ATIVO</p>
+          <FiTrash size={18} color="#FFF"/>
+      </button>
 
-
-        <button
-          className='bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2'
-        >
-            <FiTrash size={18} color="#FFF"/>
-        </button>
-
-      </article>
+    </article>
+     ))}
 
 
     </section>
